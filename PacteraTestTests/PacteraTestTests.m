7e38b0c2a768bc4e1ddca9483966eb622eb5e2d2
Prashant Gami <gamiprashant@gmail.com>
2015-01-22 23:06:04 +1100

    few more unit tests

--- PacteraTestTests/PacteraTestTests.m
+++ PacteraTestTests/PacteraTestTests.m
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 #import "PCDataDownloader.h"
+#import "PCCountryFact.h"
 
 @interface PacteraTestTests : XCTestCase
 
@@ -16,26 +17,20 @@
 
 @implementation PacteraTestTests
 
-- (void)setUp {
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+-(void)testPCCountryFact {
+    PCCountryFact *fact = [[PCCountryFact alloc] initWithDictionary:@{ @"title":@"title",
+                                                                       @"description":@"description",
+                                                                       @"imageHref":@"imageHref"}];
+    XCTAssert([fact.factTitle isEqualToString:@"title"]);
+    XCTAssert([fact.factDescription isEqualToString:@"description"]);
+    XCTAssert([fact.factImageUrl isEqualToString:@"imageHref"]);
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample {
-    // This is an example of a functional test case.
-    XCTAssert(YES, @"Pass");
-}
-
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+-(void)testEmptyPCCountryFact {
+    PCCountryFact *fact = [[PCCountryFact alloc] initWithDictionary:@{ @"title":@"",
+                                                                       @"description":@"",
+                                                                       @"imageHref":@"" }];
+    XCTAssert([fact isEmpty]);
 }
 
 -(void)testDataConnectivity {
