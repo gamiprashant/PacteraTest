6322e9ffa09617bd191d58b6c987b05e9bd3f835
Prashant Gami <gamiprashant@gmail.com>
2015-01-22 18:38:03 +1100

    Initial Commit

---
+++ PacteraTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PacteraTest
+//
+//  Created by Prashant Gami on 22/01/2015.
+//  Copyright (c) 2015 Pectera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PacteraTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PacteraTest
+//
+//  Created by Prashant Gami on 22/01/2015.
+//  Copyright (c) 2015 Pectera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
